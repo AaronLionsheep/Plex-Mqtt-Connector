81c7f888cb0d6c91a3fda60b8969144fd59efc39
Aaron <aaronr@vt.edu>
2022-02-22 19:47:03 -0500

    Make the MQTT broker endpoint configurable with environment variables

--- Mqtt-Plex-Connector.py
+++ Mqtt-Plex-Connector.py
@@ -2,23 +2,20 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import requests
 import json
+import os
 import paho.mqtt.publish as publish
 
 app = Flask(__name__)
 api = Api(app)
 
-host = 'HOST_OF_MQTT'
+host = os.environ.get('MQTT_BROKER_URI')
 topic = 'plex/'
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     data = json.loads(request.form['payload'])
-    print(data['event'])
-    publish.single(topic + data['Player']['uuid'], data['event'], hostname=host)
-    publish.single(topic + data['Player']['uuid'] + '/title', data['Metadata']['title'],
-                   hostname=host)
-    publish.single(topic + data['Player']['uuid'] + '/type', data['Metadata']['type'],
-                   hostname=host)
+    publish.single(topic + data['Player']['uuid'], data, hostname=host)
+    
     return 'OK'
 
 if __name__ == '__main__':
